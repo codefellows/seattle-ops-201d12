1592826f1c929929b81b34acd7e2949f9d00ccfc
Roger Huba <rogerhuba@gmail.com>
2024-02-20 13:44:53 -0600

    add demo

---
+++ class-02/demo.sh
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+#!/bin/bash
+
+# Script Name:                  ops201-challenge02-my-first-bash-script
+# Author Name:                  Roger Huba
+# Date of latest revision:      02/20/2024
+# Purpose:                      Print a string to the terminal
+# Resources:                    https://chat.openai.com/share/a4bd8f3e-f581-4d02-b281-9f225cceaabc
+# Declaration of variables
+# to Execute "bash ./demo.sh"
+
+greeting="Hello World, from Roger"
+
+# Main
+
+echo $greeting
+
+greeting="Hello World"
+
+echo $greeting
+
